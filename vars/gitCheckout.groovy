3baa60e48e65aa6f7cbda1a1776a9c731d6180c6
Shavin <Shavininsight@gmail.com>
2025-08-17 17:11:35 +0530

    update

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,12 +1,13 @@
 def call(Map config) {
     echo "Checking out from ${config.url} at branch ${config.branch}"
-    // if (config.branch == "main") {
-    //     checkout([
-    //         $class: 'GitSCM',
-    //         branches: [[name: "*/${config.branch}"]],
-    //         userRemoteConfigs: [[url: config.url, credentialsId: config.credentialId]]
-    //     ])
-    // } else {
-    //     echo "Checking out from ${config.branch}, which is not a valid branch"
-    // }
+    if (config.branch == "main") {
+        checkout([
+            $class: 'GitSCM',
+            branches: [[name: "*/${config.branch}"]],
+            userRemoteConfigs: [[url: config.url, 
+            credentialsId: config.credentialsId]]
+        ])
+    } else {
+        echo "Checking out from ${config.branch}, which is not a valid branch"
+    }
 }
